5fba1dd76b21e395b6b077acb5e3deb2c52896db
Arnold Omondi <arnoldomondi7@gmail.com>
2023-01-22 15:34:26 +0300

    worked on the action object

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('index.js')
+// create actions
+//WE USE CONSTANTS TO AVOID SPELLING MISTAKES.
+const CAKE_ORDERED = 'CAKE_ORDERED'
+
+//create action creator
+//CREATE THE ACTION OBJECT.
+const orderCake = () => {
+	return
+	{
+		type: CAKE_ORDERED, quantity, 1
+	}
+}
